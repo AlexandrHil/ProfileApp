40cfd3ea90dd932b1609b3ee03d961a6b3c08bcb
AlexandrHil <>
2021-03-04 20:46:40 +0300

    added first screen

---
+++ ProfileApp/ProfileApp/Views/TitleDescriptionView.swift
@@ -0,0 +1,50 @@
+//
+//  TitleDescriptionView.swift
+//  ProfileApp
+//
+//  Created by Alex on 4.03.21.
+//
+
+import UIKit
+
+@IBDesignable
+class TitleDescriptionView: UIView {
+
+    // MARK: - IBInspectable
+
+    @IBInspectable
+    var positionText: String = "" {
+        didSet {
+            self.positionLabel.text = self.positionText
+        }
+    }
+
+    @IBInspectable
+    var descriptionText: String = "" {
+        didSet {
+            self.descriptionLabel.text = self.descriptionText
+        }
+    }
+
+    // MARK: - IBOutlet
+
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var positionLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+
+    // MARK: - initialization
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.initView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        self.initView()
+    }
+
+    func initView() {
+        self.loadFromNib()
+    }
+}
