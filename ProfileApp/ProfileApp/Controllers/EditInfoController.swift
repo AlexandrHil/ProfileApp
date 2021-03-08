ba488e523cc7e063df9162aeef8248755ef65ab6
AlexandrHil <>
2021-03-09 00:50:55 +0300

    no message

--- ProfileApp/ProfileApp/Controllers/EditInfoController.swift
+++ ProfileApp/ProfileApp/Controllers/EditInfoController.swift
@@ -9,9 +9,61 @@ import UIKit
 
 class EditInfoController: UIViewController {
 
+    var userCardInfo: NPUserCardInfo?
+
+    // MARK: - IBOutlets
+
+
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var firstNameView: EditProfileView!
+    @IBOutlet weak var aliasView: EditProfileView!
+    @IBOutlet weak var lastNameView: EditProfileView!
+    @IBOutlet weak var ageView: EditProfileView!
+    @IBOutlet weak var positionView: EditProfileView!
+    @IBOutlet weak var experienceView: EditProfileView!
+    @IBOutlet weak var aboutView: EditProfileView!
+    @IBOutlet weak var aboutTextView: UITextView!
+
+    // MARK: - view life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+//        self.saveButton.addTarget(self, action: #selector(self.saveButtonTapped), for: .touchUpInside)
+
+        self.saveButton.target(
+
+        self.setViewData()
     }
 
+    // MARK: - setting view data
+
+    private func setViewData() {
+        self.firstNameView.descriptionTextField.text = self.userCardInfo?.firstName
+        self.aliasView.descriptionTextField.text = self.userCardInfo?.alias
+        self.lastNameView.descriptionTextField.text = self.userCardInfo?.lastName
+        self.ageView.descriptionTextField.text = self.userCardInfo?.age
+        self.positionView.descriptionTextField.text = self.userCardInfo?.position
+        self.experienceView.descriptionTextField.text = self.userCardInfo?.experience
+        self.aboutTextView.text = self.userCardInfo?.about
+    }
+
+    private func setModelData() {
+        guard let userModel =  self.userCardInfo else { return }
+
+        userModel.firstName = self.firstNameView.descriptionTextField.text ?? ""
+        userModel.alias = aliasView.descriptionTextField.text ?? ""
+        userModel.lastName = lastNameView.descriptionTextField.text ?? ""
+        userModel.age = ageView.descriptionTextField.text ?? ""
+        userModel.position = positionView.descriptionTextField.text ?? ""
+        userModel.experience = experienceView.descriptionTextField.text ?? ""
+        userModel.about = self.aboutTextView.text ?? ""
+    }
+
+    // MARK: - actions
+
+    @objc func saveButtonTapped() {
+        self.setModelData()
+        self.navigationController?.popViewController(animated: true)
+    }
 }
---
+++ ProfileApp/ProfileApp/Models/PAUserCardInfo.swift
@@ -0,0 +1,20 @@
+//
+//  PAUserCardInfo.swift
+//  ProfileApp
+//
+//  Created by Alex on 8.03.21.
+//
+
+import Foundation
+
+class NPUserCardInfo {
+    var firstName: String = "Yury"
+    var alias: String = "Hoy"
+    var lastName: String = "Klinskikh"
+    var age: String = "57"
+    var position: String = "Singer"
+    var experience: String = "27"
+    var about: String = """
+Soviet and Russian musician, singer, songwriter, founder and permanent leader of the "Sektor Gaza" group.
+"""
+}
